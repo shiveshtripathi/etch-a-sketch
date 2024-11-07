b13b5b8fdcd4f398340e8f5c2721dc3f7306b15f
Shivesh Kumar Tripathi <20072713+shiveshtripathi@users.noreply.github.com>
2024-11-07 19:27:04 +0530

    The height and width of cell is responsive

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const mainDiv = document.querySelector(`.grid-width`)
 
-for (i = 1; i <= 16; i++) {
+for (i = 1; i <= 10; i++) {
   const singleRowContainer = document.createElement(`div`)
   singleRowContainer.classList = `single-row`
   singleRowContainer.id = `Row No. ${i}`
   mainDiv.appendChild(singleRowContainer)
 }
 
-for (i = 1; i <= 16; i++) {
-  for (j = 1; j <= 16; j++) {
+for (i = 1; i <= 10; i++) {
+  for (j = 1; j <= 10; j++) {
     const rowDiv = document.createElement(`div`)
     rowDiv.classList = `individual-row-block`
     document.getElementById(`Row No. ${i}`).appendChild(rowDiv)
