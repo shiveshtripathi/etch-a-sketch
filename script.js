c53bff7ee4d96dfde724fe4d6ebd19e05261c112
Shivesh Kumar Tripathi <20072713+shiveshtripathi@users.noreply.github.com>
2024-11-19 13:13:22 +0530

    Add class to grid rows and columns plus css style

--- script.js
+++ script.js
@@ -4,12 +4,15 @@ const sketchGrid = document.querySelector('.sketch-grid')
 function generateGrid(gridSize) {
   for (i = 1; i <= gridSize; i++) {
     const row = document.createElement('div')
-    row.id = `Row no. ${i}`
+    row.id = `row no. ${i}`
+    row.classList = `row`
+    // row.style.display = 'flex'
     sketchGrid.appendChild(row)
     for (j = 1; j <= gridSize; j++) {
       const column = document.createElement('div')
-      column.id = `Column no. ${j}`
-      document.getElementById(`Row no. ${i}`).appendChild(column)
+      column.id = `column no. ${j}`
+      document.getElementById(`row no. ${i}`).appendChild(column)
+      column.classList = 'column'
     }
   }
 }
