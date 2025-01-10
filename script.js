5bb03a71d4f0b03ffee7c62a86963fba8bde1ab3
Shivesh Kumar Tripathi <20072713+shiveshtripathi@users.noreply.github.com>
2025-01-10 23:20:51 +0530

    Add function to display the grid size on screen

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const sketchPad = document.querySelector('.sketch-pad') //To fetch grid container
 let initialCounter = 1 //A counter to generate the grid for the first time
 const submitButton = document.getElementById('myBtn') //To fetch the button
+const displayGridSize = document.querySelector('.display-current-grid-size') //Fetch the div to display Grid Size
 
 // To generate the grid at the start
 if (initialCounter === 1) {
@@ -16,8 +17,13 @@ submitButton.addEventListener('click', () => {
 //To return the value of user input and if lower than 0 and exceeds 100  return a default value 10
 function valueSubmitBtn() {
   const gridValueByUser = document.getElementById('grid-size').value
-  if (gridValueByUser > 0 && gridValueByUser <= 100) return gridValueByUser
-  else return 10
+  if (gridValueByUser > 0 && gridValueByUser <= 100) {
+    displaySize(gridValueByUser)
+    return gridValueByUser
+  } else {
+    displaySize(gridValueByUser)
+    return 10
+  }
 }
 
 //To generate the grid by initially deleting the previous one if there is one
@@ -33,5 +39,15 @@ function generateGrid(gridSize = 10) {
       createColumn.classList.add('box')
       createRow.appendChild(createColumn)
     }
+  }
+}
+
+//To display the grid message
+function displaySize(gridSize) {
+  if (gridSize > 0 && gridSize <= 100)
+    displayGridSize.textContent = `Current grid size is ${gridSize}x${gridSize}`
+  else {
+    displayGridSize.textContent = `Your input ${gridSize} is not an accepted value. Please enter value between 1 to 100. Current grid size reset to 10x10`
+    displayGridSize.classList.add('red-text')
   }
 }
