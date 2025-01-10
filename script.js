9c32833f6603626b052f65a830295d3b68332e46
Shivesh Kumar Tripathi <20072713+shiveshtripathi@users.noreply.github.com>
2025-01-10 22:56:43 +0530

    Add logic to take user input and generate the grid Delete the previous generated grid

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-const sketchPad = document.querySelector('.sketch-pad')
+const sketchPad = document.querySelector('.sketch-pad') //To fetch grid container
+let initialCounter = 1 //A counter to generate the grid for the first time
+const submitButton = document.getElementById('myBtn') //To fetch the button
 
-function generateGrid(gridSize = 64) {
+// To generate the grid at the start
+if (initialCounter === 1) {
+  generateGrid()
+  initialCounter++
+}
+
+//To listen for button click and generate the grid based upon input
+submitButton.addEventListener('click', () => {
+  generateGrid(valueSubmitBtn())
+})
+
+//To return the value of user input and if lower than 0 and exceeds 100  return a default value 10
+function valueSubmitBtn() {
+  const gridValueByUser = document.getElementById('grid-size').value
+  if (gridValueByUser > 0 && gridValueByUser <= 100) return gridValueByUser
+  else return 10
+}
+
+//To generate the grid by initially deleting the previous one if there is one
+function generateGrid(gridSize = 10) {
+  if (sketchPad.children.length > 0) sketchPad.textContent = ''
   for (let row = 1; row <= gridSize; row++) {
     const createRow = document.createElement('div')
     createRow.classList.add('row-box')
@@ -13,5 +35,3 @@ function generateGrid(gridSize = 64) {
     }
   }
 }
-
-generateGrid()
